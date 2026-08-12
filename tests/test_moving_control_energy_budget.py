c163ee7288714ae1e0eabc65168d8a1c6ee8e58d
Kwon Dominicus <119904984+dominicus9708@users.noreply.github.com>
2026-08-12 21:06:48 +0900

    navier-stokes: test moving control-volume energy budget

---
+++ tests/test_moving_control_energy_budget.py
@@ -0,0 +1,14 @@
+import os, sys, unittest
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
+import moving_control_energy_budget as m
+
+
+class TestMovingControlEnergyBudget(unittest.TestCase):
+    def test_all(self):
+        d = m.run_checks()
+        self.assertEqual(d['passed'], d['total'])
+
+
+if __name__ == '__main__':
+    unittest.main()
